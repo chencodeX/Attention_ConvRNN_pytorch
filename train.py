92eed5e311b71ec636c569f5abd1c6b677302ece
zihao.chen <zihao.chen@moji.com>
2020-04-09 16:33:29 +0800

    debug

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
     decoder_optimizer.zero_grad()
 
     input_length = input_tensor.size(1)
-    target_length = target_tensor.size(1)-1
+    target_length = target_tensor.size(1) - 1
 
     # encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
     encoder_hidden = None
@@ -42,6 +42,8 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
 
     decoder_hidden = encoder_hidden
 
+    loss += criterion(encoder_output, target_tensor[:, 0])
+
     use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
     if use_teacher_forcing:
@@ -49,7 +51,7 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
         for di in range(target_length):
             decoder_output, decoder_hidden = decoder(
                 decoder_input, decoder_hidden)
-            loss += criterion(decoder_output, target_tensor[:, di+1])
+            loss += criterion(decoder_output, target_tensor[:, di + 1])
             decoder_input = target_tensor[:, di]  # Teacher forcing
     else:
         # Without teacher forcing: use its own predictions as the next input
@@ -59,7 +61,7 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
             # topv, topi = decoder_output.topk(1)
             decoder_input = decoder_output  # detach from history as input
 
-            loss += criterion(decoder_output, target_tensor[:, di+1])
+            loss += criterion(decoder_output, target_tensor[:, di + 1])
             # if decoder_input.item() == EOS_token:
             #     break
 
