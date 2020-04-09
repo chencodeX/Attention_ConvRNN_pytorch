a9d73d186e1d7fed94e293365331826e4c7a1ca6
zihao.chen <zihao.chen@moji.com>
2020-04-09 17:38:04 +0800

    debug

--- train.py
+++ train.py
@@ -22,6 +22,16 @@ teacher_forcing_ratio = 0.5
 
 batch_size = 32
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+        # m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
 
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
     encoder_optimizer.zero_grad()
